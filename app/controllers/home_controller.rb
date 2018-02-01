de6e85bf5ad85e4d9eed4ce8f7249c2d74460734
Jason Mintz <jasonamintz@gmail.com>
2018-02-01 12:01:12 -0500

    tweak

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,32 @@ class HomeController < ApplicationController
   def upload
     if params['file']
       file = params['file']
-      HardWorker.perform_async(file_path=file.path)
+      
+      file_path = file.path
+      
+      CSV.foreach(file_path, headers: false, encoding:'iso-8859-1:utf-8') do |row|
+        all << row
+      end
+      
+      b = Batch.create
+    
+      head = all.shift
+      head = head.map{|w|w.downcase}
+      fn = head.index('fullname')
+      bus = head.index('business')
+      sch = head.index('school')
+    
+      if fn && bus && sch
+        all.each do |a|
+          b.rows.find_or_create_by(name:a[fn],school:a[sch],business:a[bus])
+        end
+      end
+      
+      
+      
+      
+      
+      # HardWorker.perform_async(file_path=file.path)
       flash[:notice] = 'File Uploaded'
     else
       flash[:notice] = 'Error'
